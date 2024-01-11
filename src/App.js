0083b116ba915e129b171df14882f5134626c32f
yogeshjadhav17 <150352200+yogeshjadhav17@users.noreply.github.com>
2024-01-11 22:08:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import  React from "react";
+import { useForm } from "react-hook-form";
+import "./index.css";
+import  useState  from "react";
+import { EyeInvisibleOutlined,EyeOutlined } from "@ant-design/icons";
+import PasswordInput from "./components/Misc/Passwordinput";
+
+
+
+
+function App(){
+  return( <div><PasswordInput/></div>
+  );
+}
+
+export default App;
